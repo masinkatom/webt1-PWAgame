458ef51021b97f9822d7efca1f84b4493305f674
tomasinka <f1fanchannel@gmail.com>
2023-12-27 20:11:15 +0100

    ball improvements

--- ohladanceGame/js/Player.js
+++ ohladanceGame/js/Player.js
@@ -4,16 +4,20 @@ export class Player {
         this.radius = game.width * 0.065;
         this.x = game.width / 2;
         this.y = game.height * 0.8;
-        
+        this.image = document.getElementById("player");
     }
 
     update(xToUPdate) {
-        this.x += xToUPdate;
+        
+        if (this.x + xToUPdate > 0 && this.x + xToUPdate < this.game.width) {
+            this.x += xToUPdate;
+        }
     }
 
     draw(ctx) {
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
         ctx.fill();
+        ctx.drawImage(this.image, this.x - (this.radius ), this.y - (this.radius), this.radius * 2, this.radius * 2);
     }
 }

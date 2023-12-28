55c722c84702d88de98062389202cecf864bc15e
Daria Loboda <loboda.daria.sk@gmail.com>
2023-12-28 22:13:17 +0100

    tried to add sounds and music

--- ohladanceGame/js/instructions.js
+++ ohladanceGame/js/instructions.js
@@ -1,10 +1,15 @@
 // settings.js
 
-function printDescription() {
-    // Здесь вы можете добавить код для автоматической печати описания игры
-    alert("Printing game description...");
+function printDiv() {
+    var divContents = document.getElementById("credits").innerHTML;
+    var a = window.open('', '', 'height=500, width=500');
+    a.document.write('<html>');
+    a.document.write('<body > <h1>Div contents are <br>');
+    a.document.write(divContents);
+    a.document.write('</body></html>');
+    a.document.close();
+    a.print();
 }
-
 function returnToMainPage() {
     // Здесь вы можете добавить код для возврата на главную страницу
     window.location.href = "index.html";
--- ohladanceGame/js/settings.js
+++ ohladanceGame/js/settings.js
@@ -1,28 +1,87 @@
-// settings.js
-
-function toggleMusic() {
-    var musicCheckbox = document.getElementById("musicCheckbox");
-    // Здесь добавьте код для управления воспроизведением музыки в соответствии с состоянием флажка
-    if (musicCheckbox.checked) {
-        // Включить музыку
-        alert("Music turned ON");
-    } else {
-        // Выключить музыку
-        alert("Music turned OFF");
+(() => {
+    const MUSIC = new Audio('musics/test.mp3');
+    MUSIC.volume = 0.03;
+    MUSIC.loop = true;
+  
+    const MUSIC_STORAGE = 'misw-settings-music';
+    let musicEnabled = JSON.parse(window.localStorage.getItem(MUSIC_STORAGE));
+    musicEnabled = musicEnabled === null ? true : musicEnabled;
+  
+    const SOUND = new Audio('musics/test2.mp3');
+    SOUND.volume = 0.1;
+  
+    const SOUND_STORAGE = 'misw-settings-sound';
+    let soundEnabled = JSON.parse(window.localStorage.getItem(SOUND_STORAGE));
+    soundEnabled = soundEnabled === null ? true : soundEnabled;
+    
+  
+    window.music = {
+      play: MUSIC.play,
+      pause: MUSIC.pause,
+      getValue() {
+        return musicEnabled;
+      },
+      handler(value) {
+        musicEnabled = value;
+        window.localStorage.setItem(MUSIC_STORAGE, musicEnabled);
+  
+        if (value)
+          MUSIC.play();
+        else
+          MUSIC.pause();
+      }
     }
-}
-
-function toggleSound() {
-    var soundCheckbox = document.getElementById("soundCheckbox");
-    // Здесь добавьте код для управления воспроизведением звуковых эффектов в соответствии с состоянием флажка
-    if (soundCheckbox.checked) {
-        // Включить звук
-        alert("Sound Effects turned ON");
-    } else {
-        // Выключить звук
-        alert("Sound Effects turned OFF");
+  
+    window.sound = {
+      play: () => {
+        if (!soundEnabled)
+          return;
+  
+        SOUND.currentTime = 0;
+        SOUND.play();
+      },
+      getValue() {
+        return soundEnabled;
+      },
+      handler(value) {
+        soundEnabled = value;
+        window.localStorage.setItem(SOUND_STORAGE, soundEnabled);
+        if (!soundEnabled)
+          SOUND.pause();
+      }
     }
-}
+  
+    
+    MUSIC.addEventListener('canplaythrough', () => {
+      const musicAutostart = setInterval(() => {
+        if (!musicEnabled)
+          return clearInterval(musicAutostart);
+        MUSIC.play()
+          .then(() => clearInterval(musicAutostart))
+          .catch(() => {});
+      }, 100);
+    });
+  
+  })();
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const musicCheckbox = document.getElementById('musicCheckbox');
+    const soundCheckbox = document.getElementById('soundCheckbox');
+
+    // Установите начальные значения чекбоксов
+    musicCheckbox.checked = window.music.getValue();
+    soundCheckbox.checked = window.sound.getValue();
+
+    // Добавьте обработчики событий для чекбоксов
+    musicCheckbox.addEventListener('change', function () {
+        window.music.handler(this.checked);
+    });
+
+    soundCheckbox.addEventListener('change', function () {
+        window.sound.handler(this.checked);
+    });
+});
+
 
 function returnToMainPage() {
     // Здесь вы можете добавить код для возврата на главную страницу

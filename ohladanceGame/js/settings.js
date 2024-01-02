3da855071d3eac1df28ec45fc394368da53b0b24
Tomáš Adamko <tomas.adamko395@gmail.com>
2024-01-02 12:14:57 +0100

    music error fix

--- ohladanceGame/js/settings.js
+++ ohladanceGame/js/settings.js
@@ -12,8 +12,30 @@ function resetGame() {
 }
 
 if (localStorage.getItem("maxLevel") === null) {
-  localStorage.setItem("maxLevel", 1);}
-else{
-    localStorage.setItem("maxLevel", 1)
-  }
+  localStorage.setItem("maxLevel", 1);
+}
+else {
+  localStorage.setItem("maxLevel", 1)
+}
 
+document.addEventListener('DOMContentLoaded', function () {
+  const musicCheckbox = document.getElementById('musicCheckbox');
+  const soundCheckbox = document.getElementById('soundCheckbox');
+
+
+  musicCheckbox.checked = window.music.getValue();
+  soundCheckbox.checked = window.sound.getValue();
+
+
+  musicCheckbox.addEventListener('change', function () {
+    window.music.handler(this.checked);
+  });
+
+  soundCheckbox.addEventListener('change', function () {
+    window.sound.handler(this.checked);
+  });
+  soundCheckbox.addEventListener('change', function () {
+    console.log('Sound Checkbox Changed:', this.checked);
+    window.sound.handler(this.checked);
+  });
+});

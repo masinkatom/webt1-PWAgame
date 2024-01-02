67a68e1a35d2b246da8e5d67e70e7d794c975adb
Daria Loboda <loboda.daria.sk@gmail.com>
2024-01-02 11:26:08 +0100

    reset hry

--- ohladanceGame/js/settings.js
+++ ohladanceGame/js/settings.js
@@ -3,3 +3,17 @@ function returnToMainPage() {
   window.location.href = "index.html";
 }
 
+function resetGame() {
+  // Funkcia na vymazanie uloženého stavu hry
+  localStorage.removeItem('gameState');
+  // Reset hodnôt checkboxov
+  document.getElementById('musicCheckbox').checked = true;
+  document.getElementById('soundCheckbox').checked = true;
+}
+
+if (localStorage.getItem("maxLevel") === null) {
+  localStorage.setItem("maxLevel", 1);}
+else{
+    localStorage.setItem("maxLevel", 1)
+  }
+
